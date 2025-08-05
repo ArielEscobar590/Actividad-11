afd73cff9e3f925c25de9ae994687fb5bc4bd249
German Escobar <germanjosearielescobarescobar@gmail.com>
2025-08-04 19:32:47 -0600

    Actividad 11

--- Quick Short con Diccionario.py
+++ Quick Short con Diccionario.py
@@ -2,10 +2,10 @@ def quick_sort(lista):
     if len(lista) <= 1:
         return lista
 
-    pivote = lista[carnet]
-    menores = [x for x in lista[carnet][1:] if x < pivote]
-    iguales = [x for x in lista if x == pivote]
-    mayores = [x for x in lista[carnet][1:] if x > pivote]
+    pivote = lista[carnet[0]]
+    menores = [x for x in lista[carnet[1:]] if x < pivote]
+    iguales = [x for x in lista[carnet] if x == pivote]
+    mayores = [x for x in lista[carnet[1:]] if x > pivote]
 
     return quick_sort(menores) + iguales + quick_sort(mayores)
 
@@ -14,7 +14,7 @@ cantidad = int(input("¿Cuántos estudiantes desea ingresar? "))
 
 for i in range(cantidad):
     print(f"\nEstudiante #{i + 1}")
-    carnet = input("Ingrese el número de carnet: ")
+    carnet = int(input("Ingrese el número de carnet: "))
     nombre = input("Ingrese el nombre completo: ")
     edad = int(input("Ingrese la edad: "))
     carrera = input("Ingrese la carrera: ")
